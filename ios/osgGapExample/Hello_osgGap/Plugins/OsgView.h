b4068b402b6e16787a062dc20f30f98ba5ff9fc1
osgGap <sylvia9126@gmail.com>
2013-09-24 18:24:37 +0900

    iOS version Commit
    osgGap-1.0.0.js Initailize Commit

---
+++ ios/osgGapExample/Hello_osgGap/Plugins/OsgView.h
@@ -0,0 +1,36 @@
+//
+//  OsgView.h
+//  Hello_osgGap
+//
+//  Created by Youna on 13. 9. 16..
+//
+//
+
+#import <OpenGLES/EAGL.h>
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import <OpenGLES/ES1/gl.h>
+#import <OpenGLES/ES1/glext.h>
+
+@interface OsgView : UIView
+{
+    EAGLContext* m_context;
+}
+
+
++(Class) layerClass;
+
+-(id) initWithWindow:(UIWindow*)window;
+
+-(unsigned int) getWebViewPtr;
+
+-(BOOL) webView_setSceneData:(int)_rootPtr;
+
+-(unsigned int) getGroupNode;
+-(unsigned int) getGeodeNode;
+-(unsigned int) getMatrixTransformNode;
+
+-(BOOL) matrixTransform_addChild:(int)_matrixTransform geodePtr:(int)_ptr;
+-(BOOL) group_addChild:(int)_groupPtr geodePtr:(int)_ptr;
+
+@end
